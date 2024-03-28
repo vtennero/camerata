afd003c1636601ce11959a8ff93b00f2b6d87d09
vtennero <victor.tenneroni@gmail.com>
2024-03-29 00:09:30 +0800

    removed api files

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination: "http://localhost:5000/:path*",
-//       },
-//     ];
-//   },
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
